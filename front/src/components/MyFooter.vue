e9ee113acc9b7df3f49f70860d31715c244a3704
laura-lpn <lauralepannetier@gmail.com>
2024-06-19 18:10:06 +0200

    refactor: Update image paths in footer and header components

--- front/src/components/MyFooter.vue
+++ front/src/components/MyFooter.vue
@@ -4,7 +4,7 @@
   w-full py-4 px-28 flex flex-col justify-center gap-4 md:flex-row md:gap-16 items-center mt-auto
   ">
     <RouterLink :to="{name : 'home'}" class="hover:animate-pulse">
-      <img src="/logo.svg" alt="logo KitUI" class="h-12">
+      <img src="@/assets/logo.svg" alt="logo KitUI" class="h-12">
     </RouterLink>
     <p class="text-medium text-black">© Copyright KitUI 2024</p>
     <RouterLink :to="{name : 'legals'}" class="text-pink">Mentions légales</RouterLink>
--- front/src/components/MyHeader.vue
+++ front/src/components/MyHeader.vue
@@ -7,7 +7,7 @@
   flex justify-center items-center
   ">
     <RouterLink :to="{ name: 'home'}" title="Accueil" class="hover:animate-pulse z-[100]">
-      <img src="/logo.svg" alt="logo KitUI" class="h-16">
+      <img src="@/assets/logo.svg" alt="logo KitUI" class="h-16">
     </RouterLink>
   </header>
 </template>
