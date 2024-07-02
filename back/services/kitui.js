fb57f41d92c9c7cacd3ed163c824f4210eba255e
laura-lpn <74555001+laura-lpn@users.noreply.github.com>
2024-07-02 12:09:00 +0200

    lint

--- back/services/kitui.js
+++ back/services/kitui.js
@@ -56,81 +56,79 @@ async function create(newKitui) {
 
     imports = fontTextImport === fontTitleImport ? fontTextImport : fontTextImport + '\n' + fontTitleImport + '\n';
 
-    const styleRoot = `
-    ${imports}
-    :root {
-      --layout-max-width : ${newKitui.layout.maxWidth}px;
-      --layout-padding-x : ${newKitui.layout.padding.x}px;
-      --layout-padding-y : ${newKitui.layout.padding.y}px;
-      --layout-nb-columns : ${newKitui.layout.numColumns};
-      --layout-gutter : ${newKitui.layout.gutter}px;
+    const styleRoot = `${imports}
+:root {
+  --layout-max-width : ${newKitui.layout.maxWidth}px;
+  --layout-padding-x : ${newKitui.layout.padding.x}px;
+  --layout-padding-y : ${newKitui.layout.padding.y}px;
+  --layout-nb-columns : ${newKitui.layout.numColumns};
+  --layout-gutter : ${newKitui.layout.gutter}px;
 
-      --color-dark-base : ${newKitui.colors.dark.base};
-      --color-dark-lighter : ${newKitui.colors.dark.lighter};
-      --color-dark-darker : ${newKitui.colors.dark.darker};
-      --color-light-base : ${newKitui.colors.light.base};
-      --color-light-lighter : ${newKitui.colors.light.lighter};
-      --color-light-darker : ${newKitui.colors.light.darker};
-      --color-accent : ${newKitui.colors.accent};
+  --color-dark-base : ${newKitui.colors.dark.base};
+  --color-dark-lighter : ${newKitui.colors.dark.lighter};
+  --color-dark-darker : ${newKitui.colors.dark.darker};
+  --color-light-base : ${newKitui.colors.light.base};
+  --color-light-lighter : ${newKitui.colors.light.lighter};
+  --color-light-darker : ${newKitui.colors.light.darker};
+  --color-accent : ${newKitui.colors.accent};
 
-      --title-font-family : '${newKitui.typography.fontFamily.title}', sans-serif;
-      --h1-font-size : ${newKitui.typography.style.h1.size}px;
-      --h1-font-weight : ${newKitui.typography.style.h1.weight};
-      --h2-font-size : ${newKitui.typography.style.h2.size}px;
-      --h2-font-weight : ${newKitui.typography.style.h2.weight};
-      --h3-font-size : ${newKitui.typography.style.h3.size}px;
-      --h3-font-weight : ${newKitui.typography.style.h3.weight};
-      --h4-font-size : ${newKitui.typography.style.h4.size}px;
-      --h4-font-weight : ${newKitui.typography.style.h4.weight};
-      --h5-font-size : ${newKitui.typography.style.h5.size}px;
-      --h5-font-weight : ${newKitui.typography.style.h5.weight};
-      --h6-font-size : ${newKitui.typography.style.h6.size}px;
-      --h6-font-weight : ${newKitui.typography.style.h6.weight};
+  --title-font-family : '${newKitui.typography.fontFamily.title}', sans-serif;
+  --h1-font-size : ${newKitui.typography.style.h1.size}px;
+  --h1-font-weight : ${newKitui.typography.style.h1.weight};
+  --h2-font-size : ${newKitui.typography.style.h2.size}px;
+  --h2-font-weight : ${newKitui.typography.style.h2.weight};
+  --h3-font-size : ${newKitui.typography.style.h3.size}px;
+  --h3-font-weight : ${newKitui.typography.style.h3.weight};
+  --h4-font-size : ${newKitui.typography.style.h4.size}px;
+  --h4-font-weight : ${newKitui.typography.style.h4.weight};
+  --h5-font-size : ${newKitui.typography.style.h5.size}px;
+  --h5-font-weight : ${newKitui.typography.style.h5.weight};
+  --h6-font-size : ${newKitui.typography.style.h6.size}px;
+  --h6-font-weight : ${newKitui.typography.style.h6.weight};
 
-      --text-font-family : '${newKitui.typography.fontFamily.text}', sans-serif;
-      --text-font-size : ${newKitui.typography.style.text}px;
+  --text-font-family : '${newKitui.typography.fontFamily.text}', sans-serif;
+  --text-font-size : ${newKitui.typography.style.text}px;
 
-      --button-padding-x : ${newKitui.buttons.padding.x}px;
-      --button-padding-y : ${newKitui.buttons.padding.y}px;
-      --button-border-radius : ${newKitui.buttons.borderRadius}px;
-      --button-font-size : ${newKitui.buttons.fontSize}px;
-      --button-color-text: ${newKitui.buttons.colorText};
+  --button-padding-x : ${newKitui.buttons.padding.x}px;
+  --button-padding-y : ${newKitui.buttons.padding.y}px;
+  --button-border-radius : ${newKitui.buttons.borderRadius}px;
+  --button-font-size : ${newKitui.buttons.fontSize}px;
+  --button-color-text: ${newKitui.buttons.colorText};
 
-      --input-padding-x : ${newKitui.inputs.padding.x}px;
-      --input-padding-y : ${newKitui.inputs.padding.y}px;
-      --input-border-radius : ${newKitui.inputs.borderRadius}px;
-      --input-font-size : ${newKitui.inputs.fontSize}px;
+  --input-padding-x : ${newKitui.inputs.padding.x}px;
+  --input-padding-y : ${newKitui.inputs.padding.y}px;
+  --input-border-radius : ${newKitui.inputs.borderRadius}px;
+  --input-font-size : ${newKitui.inputs.fontSize}px;
 
-      --card-padding-x : ${newKitui.cards.padding.x}px;
-      --card-padding-y : ${newKitui.cards.padding.y}px;
-      --card-border-radius : ${newKitui.cards.borderRadius}px;
-  }
-  `
+  --card-padding-x : ${newKitui.cards.padding.x}px;
+  --card-padding-y : ${newKitui.cards.padding.y}px;
+  --card-border-radius : ${newKitui.cards.borderRadius}px;
+}
+`
 
   const breakpoints = () => {
     let styles = '';
   
     for (let i = 1; i <= newKitui.layout.numColumns; i++) {
       styles += `
-        .col-${i} {
-          grid-column: span ${i};
-        }
+.col-${i} {
+  grid-column: span ${i};
+}
       `;
     }
   
     newKitui.layout.breakpoints.forEach((breakpoint) => {
       styles += `
-        @media screen and (min-width: ${breakpoint.value}px) {
+@media screen and (min-width: ${breakpoint.value}px) {
       `;
       for (let i = 1; i <= newKitui.layout.numColumns; i++) {
         styles += `
-          .col-${breakpoint.name}-${i} {
-            grid-column: span ${i};
-          }
+  .col-${breakpoint.name}-${i} {
+    grid-column: span ${i};
+  }
         `;
       }
-      styles += `
-        }
+      styles += `}
       `;
     });
   
