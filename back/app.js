b9f919efee5f9a7605e8b24455b6638b678ecef8
laura-lpn <lauralepannetier@gmail.com>
2025-03-24 14:19:27 +0100

    fix: Remove unnecessary console logs in kitui.js and clean up app.js

--- back/app.js
+++ back/app.js
@@ -9,9 +9,3 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use('/api', routes);
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Serveur démarré sur le port ${port}`);
-});
--- back/controllers/kitui.js
+++ back/controllers/kitui.js
@@ -2,8 +2,6 @@ const kituiService = require('../services/kitui');
 
 async function create(req, res) {
   const kitui = await kituiService.create(req.body);
-  console.log(req.body);
-  console.log(kitui);
   if (kitui)
     res.status(201).json({ message: "Kitui créé", path: kitui });
   else
